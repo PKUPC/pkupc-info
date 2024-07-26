b6718b4ee8dfddf4cb04cc24a9b9cb0d0c81bbff
kinami <kinami0331@outlook.com>
2024-07-27 06:49:52 +0800

    添加 GitHub Actions 用于自动部署 GitHub Pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/kinami0331/pnku-info',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/kinami0331/pnku-info',
         },
         theme: {
           customCss: './src/css/custom.css',
