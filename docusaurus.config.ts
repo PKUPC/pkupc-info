8147915275be05334e3d52a3107b9c44b2162694
kinami <36267804+kinami0331@users.noreply.github.com>
2024-07-27 07:30:53 +0800

    修复 GitHub Pages 中的 baseUrl 问题。 (#2)
    (cherry picked from commit 9af2ddbb2aec3ee685b542a5f0f4b1f1154331ba)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,31 +2,26 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+const githubPageMode = process.env.GITHUB_PAGE_MODE?.toUpperCase() === "TRUE";
+
 const config: Config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://info.pkupuzzle.art/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  baseUrl: githubPageMode ? "/pnku-info/" : "/",
+
+  organizationName: 'P&KU Team',
+  projectName: 'P&KU Info',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hans',
+    locales: ['zh-Hans'],
   },
 
   presets: [
@@ -35,17 +30,11 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/kinami0331/pnku-info',
+          editUrl: 'https://github.com/kinami0331/pnku-info',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/kinami0331/pnku-info',
+          editUrl: 'https://github.com/kinami0331/pnku-info',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -72,7 +61,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://kinami0331.github.io/pnku-info/',
           label: 'GitHub',
           position: 'right',
         },
