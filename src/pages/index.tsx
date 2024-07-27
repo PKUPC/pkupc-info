5adbb6d03e3001218da57f5b34ac46138211438d
kinami <kinami0331@outlook.com>
2024-07-27 09:29:03 +0800

    修复换行符问题。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
-
 import styles from './index.module.css';
 
 function HomepageHeader() {
