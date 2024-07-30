a94cdf8bb1c84a8e6b74e75412900a193b507877
Jimmy Wang <wangjingheng0625@gmail.com>
2024-07-30 00:06:47 -0700

    将防剧透组件提升到全局环境，格式化。

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,7 +1,9 @@
 import MDXComponents from '@theme-original/MDXComponents';
 import CustomImage from '@site/src/components/CustomImage';
+import AnswerBlock from '@site/src/components/AnswerBlock';
 
 export default {
     ...MDXComponents,
     CustomImage,
+    AnswerBlock,
 };
