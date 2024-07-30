8b6b221ccf050e4c03c4a7a51120a37eb6385064
kinami <36267804+kinami0331@users.noreply.github.com>
2024-07-30 08:16:54 +0800

    引入 tailwind css。

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+// see https://dev.to/shannonajclarke/using-tailwindcss-v3-in-docusaurus-in-5-steps-5c26
+module.exports = {
+    content: ['./src/**/*.{js,jsx,ts,tsx,md,mdx}', './docs/**/*.{md,mdx}', './blog/**/*.{md,mdx}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    darkMode: ['class', '[data-theme="dark"]'],
+    corePlugins: {
+        preflight: false,
+    },
+    blocklist: ['container'],
+};
