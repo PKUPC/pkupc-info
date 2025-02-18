0336a378f8a5fbcbecbe0768f1f92fab90ea153e
Jimmy Wang <33430853+JimmyCHWang@users.noreply.github.com>
2025-02-18 00:33:58 -0800

    feat: theme change and thumb image added

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -67,7 +67,7 @@ const config: Config = {
 
     themeConfig: {
         // Replace with your project's social card
-        image: 'img/docusaurus-social-card.jpg',
+        image: 'img/info-thumb-image.jpg',
         navbar: {
             title: 'PKU Puzzle Club',
             logo: {
