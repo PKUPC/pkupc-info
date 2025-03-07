08ed83881f6333af1bfeb1dfaad14fc9e0054b42
Jimmy Wang <33430853+JimmyCHWang@users.noreply.github.com>
2025-03-07 01:40:05 -0800

    feat: add zhibi 001 and 011 as samples

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 // see https://dev.to/shannonajclarke/using-tailwindcss-v3-in-docusaurus-in-5-steps-5c26
 module.exports = {
-    content: ['./src/**/*.{js,jsx,ts,tsx,md,mdx}', './docs/**/*.{md,mdx}', './blog/**/*.{md,mdx}'],
+    content: ['./src/**/*.{js,jsx,ts,tsx,md,mdx}', './docs/**/*.{md,mdx}', './content/**/*.{md,mdx}'],
     theme: {
         extend: {
             screens: {
